c1834b447e1b9e5d6b981628913e65318132d860
Nikolai <nikolai-kiefer@web.de>
2021-10-13 08:06:40 +0200

    Added a Header Component

--- Event-Manager-App/src/app/app.module.ts
+++ Event-Manager-App/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MediaGalleryComponent } from './media-gallery/media-gallery.component';
 import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    MediaGalleryComponent
+    MediaGalleryComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
